a49b195703acfc83f2b2333d831c5abd45b40442
Hilmi AM <imiia75775@proton.me>
2025-07-27 16:21:06 +0700

    feat: logical op

---
+++ LogicalOperators.php
@@ -0,0 +1,22 @@
+<?php
+
+// AND
+var_dump(true && true);
+var_dump(true && false);
+var_dump(true and true);
+var_dump(true and false);
+
+// OR
+var_dump(true || false);
+var_dump(true || true);
+var_dump(true or false);
+var_dump(false or false);
+
+// XOR
+var_dump(true xor false);
+var_dump(true xor true);
+var_dump(false xor false);
+
+// NOT
+var_dump(!true);
+var_dump(!false);
