85058802e9cb1361547a3a0783728c10841bd383
Hilmi AM <imiia75775@proton.me>
2025-07-27 19:17:30 +0700

    feat: comment

---
+++ Comments.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+This is a multi-line comment.
+The following function prints "Hello".
+*/
+function sayHello()
+{
+    // Single-line comment: Print a greeting
+    echo "Hello" . PHP_EOL;
+}
+
+// Calling the sayHello function
+sayHello();
