86e22576af7342f286e897213b481d0f3bc17614
Hilmi AM <imiia75775@proton.me>
2025-07-27 15:37:10 +0700

    feat: const

---
+++ Constant.php
@@ -0,0 +1,18 @@
+<?php
+
+define("AUTHOR", "Noir");
+define("APP_VERSION", 100);
+
+const APP = "AN SI B";
+
+echo "Author: ";
+echo AUTHOR;
+echo "\n";
+
+echo "App Version: ";
+echo APP_VERSION;
+echo "\n";
+
+echo "App : ";
+echo APP;
+echo "\n";
