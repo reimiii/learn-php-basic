d52f9bb4cba834baee1b3bee191f2c2e02664399
Hilmi AM <imiia75775@proton.me>
2025-07-27 15:32:49 +0700

    feat: variable

---
+++ Variable.php
@@ -0,0 +1,22 @@
+<?php
+$name = "John";
+$age = 30;
+
+echo "Name: ";
+echo $name;
+echo "\n";
+
+echo "Age: ";
+echo $age;
+echo "\n";
+
+$example = "john";
+$$example = "carter";
+
+echo "Example: ";
+echo $example;
+echo "\n";
+
+echo "john: ";
+echo $john;
+echo "\n";
